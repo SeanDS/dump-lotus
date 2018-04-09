5e36a30aaab80fe1c0de50b083565b37fe168581
Sean Leavey <gpg@attackllama.com>
2018-04-09 22:22:06 +0200

    Update example

--- example.py
+++ example.py
@@ -3,21 +3,19 @@ import logging
 from lotus.parse import LotusParser
 
 # set up logger
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(message)s'))
-logger = logging.getLogger()
-logger.addHandler(handler)
-logger.setLevel(logging.WARNING)
+logging.basicConfig()
 
-root_path = "/path/to/scraped/tree"
+# path to scraped Lotus Notes pages
+root_dir = "/path/to/scraped/tree"
 
-parser = LotusParser(root_path)
-parser.parse_all()
+# path to archive XML versions of Lotus Notes pages
+archive_dir = "archive"
 
-# check all images in pages were found
-for post in list(parser.pages):
-    for url in post.internal_urls:
-        if url not in [page.path for page in parser.pages]:
-            # not in main list, but is it an alias?
-            if url not in parser.page_path_aliases.keys():
-                print("%s not found (from %s)" % (url, post.path))
+# set up parser
+parser = LotusParser(root_dir, archive_dir)
+
+# find Lotus objects
+parser.find()
+
+# save to archive
+parser.archive()
